b8d9bf3b17b664bea9a9a24c36281c5dee5a5ab8
YourStepDada <117860806+YourStepDada@users.noreply.github.com>
2023-06-30 20:07:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Mohammad";
+var last_name = "Shaikhzada";
+var a = 10;
+var b = 5;
+var full_name = first_name.concat(last_name);
+console.log(first_name)
+console.log(last_name)
+var add = a + b;
+console.log(add);
+console.log(full_name);
+console.log("This is the console");
+console.log("Click here to get nothing");
